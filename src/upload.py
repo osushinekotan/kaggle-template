3dca222de791aa2f4c129f7206b116e60e3c87d8
osushinekotan <spmt3710@gmail.com>
2024-12-22 17:43:28 +0900

    Add upload script for dataset and artifact management with Kaggle integration

---
+++ src/upload.py
@@ -0,0 +1,41 @@
+import logging
+import os
+
+import fire
+import rootutils
+
+from kaggle_utils.dataset import dataset_upload
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+ROOT_DIR = rootutils.setup_root(".", indicator="pyproject.toml", cwd=True, dotenv=True)
+DATA_DIR = ROOT_DIR / "data"
+INPUT_DIR = DATA_DIR / "input"
+OUTPUT_DIR = DATA_DIR / "output"
+INPUT_DIR.mkdir(exist_ok=True, parents=True)
+
+KAGGLE_USERNAME = os.getenv("KAGGLE_USERNAME")
+KAGGLE_COMPETITION_NAME = os.getenv("KAGGLE_COMPETITION_NAME", "spaceship-titanic")
+
+assert KAGGLE_USERNAME, "KAGGLE_USERNAME is not set."
+
+
+ARTIFACTS_HANDLE = f"{KAGGLE_USERNAME}/{KAGGLE_COMPETITION_NAME}-artifacts"
+CODES_HANDLE = f"{KAGGLE_USERNAME}/{KAGGLE_COMPETITION_NAME}-codes"
+
+if __name__ == "__main__":
+    fire.Fire(
+        {
+            "codes": lambda: dataset_upload(
+                handle=CODES_HANDLE,
+                local_dataset_dir=ROOT_DIR,
+                update=True,
+            ),
+            "artifacts": lambda exp_name: dataset_upload(
+                handle=ARTIFACTS_HANDLE,
+                local_dataset_dir=OUTPUT_DIR / exp_name,  # output dir に存在する artifact をアップロード
+                update=False,
+            ),
+        }
+    )
