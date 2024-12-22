1f3686e71f87458aed635c3bc8366b1d20f4d7a2
osushinekotan <spmt3710@gmail.com>
2024-12-22 17:43:22 +0900

    Enhance sample_submission script with output directory logging and dataset upload functionality

--- experiments/001/sample_submission.py
+++ experiments/001/sample_submission.py
@@ -3,3 +3,23 @@ import polars as pl
 
 df = pl.read_csv(config.COMP_DATASET_DIR / "sample_submission.csv")
 df.write_csv(config.OUTPUT_DIR / "submission.csv")
+
+print(config.OUTPUT_DIR)
+print(pl.read_csv(config.OUTPUT_DIR / "submission.csv").shape)
+
+print(config.ARTIFACT_DIR)
+print(pl.read_csv(config.ARTIFACT_DIR / "submission.csv").shape)
+
+if not config.IS_KAGGLE_ENV:
+    from src.kaggle_utils.dataset import dataset_upload
+
+    dataset_upload(
+        handle=config.ARTIFACTS_HANDLE,
+        local_dataset_dir=config.OUTPUT_DIR,
+        update=False,
+    )
+    dataset_upload(
+        handle=config.CODES_HANDLE,
+        local_dataset_dir=config.ROOT_DIR,
+        update=True,
+    )
