1cc311a6261f75515921c55b153e3e833645c8bd
osushinekotan <spmt3710@gmail.com>
2024-12-23 19:37:29 +0900

    Refactor upload logic in code.ipynb and inference.py to integrate model handling and update parameters

--- {{cookiecutter.project_slug}}/experiments/001/inference.py
+++ {{cookiecutter.project_slug}}/experiments/001/inference.py
@@ -10,16 +10,16 @@ print(pl.read_csv(config.OUTPUT_DIR / "submission.csv").shape)
 print(config.ARTIFACT_DIR)
 print(pl.read_csv(config.ARTIFACT_DIR / "submission.csv").shape)
 
-if not config.IS_KAGGLE_ENV:
-    from src.kaggle_utils.dataset import dataset_upload
+# if not config.IS_KAGGLE_ENV:
+#     from src.kaggle_utils.customhub import dataset_upload, model_upload
 
-    dataset_upload(
-        handle=config.ARTIFACTS_HANDLE,
-        local_dataset_dir=config.OUTPUT_DIR,
-        update=True,
-    )
-    dataset_upload(
-        handle=config.CODES_HANDLE,
-        local_dataset_dir=config.ROOT_DIR,
-        update=True,
-    )
+#     model_upload(
+#         handle=config.ARTIFACTS_HANDLE,
+#         local_model_dir=config.OUTPUT_DIR,
+#         update=False,
+#     )
+#     dataset_upload(
+#         handle=config.CODES_HANDLE,
+#         local_dataset_dir=config.ROOT_DIR,
+#         update=True,
+#     )
