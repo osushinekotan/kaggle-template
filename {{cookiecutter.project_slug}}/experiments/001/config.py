e35048aa13f24a2072e0859ad6f5db45fe84d9c5
osushinekotan <spmt3710@gmail.com>
2024-12-23 19:23:09 +0900

    Refactor ARTIFACT_DIR path in config.py for improved clarity

--- {{cookiecutter.project_slug}}/experiments/001/config.py
+++ {{cookiecutter.project_slug}}/experiments/001/config.py
@@ -34,7 +34,7 @@ else:
     ROOT_DIR = Path("/kaggle/working")
     INPUT_DIR = Path("/kaggle/input")
     # 当該 code 以外の生成物が格納されている場所 (Model として使用できる)  ARTIFACT_DIR / EXP_NAME / 1 でアクセス可能
-    ARTIFACT_DIR = INPUT_DIR / "other" / f"{KAGGLE_COMPETITION_NAME}-artifacts"
+    ARTIFACT_DIR = INPUT_DIR  / f"{KAGGLE_COMPETITION_NAME}-artifacts"/ "other"
     OUTPUT_DIR = ROOT_DIR  # 当該 code の生成物の出力先
 
 COMP_DATASET_DIR = INPUT_DIR / KAGGLE_COMPETITION_NAME
