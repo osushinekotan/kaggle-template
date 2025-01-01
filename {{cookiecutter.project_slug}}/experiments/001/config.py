2b8526023c37220e9f3ec96ab98b5b6e384a13da
osushinekotan <spmt3710@gmail.com>
2025-01-01 20:47:07 +0900

    Update config.py to set default Kaggle competition name and username, and adjust artifact directory structure

--- {{cookiecutter.project_slug}}/experiments/001/config.py
+++ {{cookiecutter.project_slug}}/experiments/001/config.py
@@ -6,9 +6,8 @@ EXP_NAME = "001"
 
 # ---------- # DIRECTORIES # ---------- #
 IS_KAGGLE_ENV = os.getenv("KAGGLE_DATA_PROXY_TOKEN") is not None
-KAGGLE_COMPETITION_NAME = os.getenv(
-    "KAGGLE_COMPETITION_NAME", "{{ cookiecutter.competition_name }}"
-)
+KAGGLE_COMPETITION_NAME = os.getenv("KAGGLE_COMPETITION_NAME", "equity-post-HCT-survival-predictions")
+KAGGLE_USERNAME = os.getenv("KAGGLE_USERNAME", "mst8823")
 
 if not IS_KAGGLE_ENV:
     import rootutils
@@ -25,16 +24,13 @@ if not IS_KAGGLE_ENV:
     # 当該 code の生成物の出力先. kaggle code とパスを合わせるために 1 を付与
     OUTPUT_DIR = ARTIFACT_DIR / EXP_NAME / "1"
 
-    KAGGLE_USERNAME = os.getenv("KAGGLE_USERNAME", "{{ cookiecutter.kaggle_username }}")
-    ARTIFACTS_HANDLE = (
-        f"{KAGGLE_USERNAME}/{KAGGLE_COMPETITION_NAME}-artifacts/other/{EXP_NAME}"
-    )
+    ARTIFACTS_HANDLE = f"{KAGGLE_USERNAME}/{KAGGLE_COMPETITION_NAME}-artifacts/other/{EXP_NAME}"
     CODES_HANDLE = f"{KAGGLE_USERNAME}/{KAGGLE_COMPETITION_NAME}-codes"
 else:
     ROOT_DIR = Path("/kaggle/working")
     INPUT_DIR = Path("/kaggle/input")
     # 当該 code 以外の生成物が格納されている場所 (Model として使用できる)  ARTIFACT_DIR / EXP_NAME / 1 でアクセス可能
-    ARTIFACT_DIR = INPUT_DIR  / f"{KAGGLE_COMPETITION_NAME.lower()}-artifacts"/ "other"
+    ARTIFACT_DIR = INPUT_DIR / "m" / KAGGLE_USERNAME / f"{KAGGLE_COMPETITION_NAME.lower()}-artifacts" / "other"
     OUTPUT_DIR = ROOT_DIR  # 当該 code の生成物の出力先
 
 COMP_DATASET_DIR = INPUT_DIR / KAGGLE_COMPETITION_NAME
